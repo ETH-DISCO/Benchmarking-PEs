973761913115f9c224f28d59996de0225610cfc3
JiaX-TCS <jiaxie@student.ethz.ch>
2024-11-25 11:23:07 +0100

    add expressiveness dataset and loading process

--- grit/loader/master_loader.py
+++ grit/loader/master_loader.py
@@ -52,6 +52,261 @@ from grit.transform.dist_transforms import (add_dist_features, add_reverse_edges
                                                  add_self_loops, effective_resistances,
                                                  effective_resistance_embedding,
                                                  effective_resistances_from_embedding)
+from torch_geometric.data import DataLoader, Data
+from torch_geometric.utils import degree
+from torch_geometric.utils.convert import from_networkx
+import networkx as nx
+
+# Synthetic datasets
+
+class SymmetrySet:
+    def __init__(self):
+        self.hidden_units = 0
+        self.num_classes = 0
+        self.num_features = 0
+        self.num_nodes = 0
+
+    def addports(self, data):
+        data.ports = torch.zeros(data.num_edges, 1)
+        degs = degree(data.edge_index[0], data.num_nodes, dtype=torch.long)  # out degree of all nodes
+        for n in range(data.num_nodes):
+            deg = degs[n]
+            ports = np.random.permutation(int(deg))
+            for i, neighbor in enumerate(data.edge_index[1][data.edge_index[0] == n]):
+                nb = int(neighbor)
+                data.ports[torch.logical_and(data.edge_index[0] == n, data.edge_index[1] == nb), 0] = float(ports[i])
+        return data
+
+    def makefeatures(self, data):
+        data.x = torch.ones((data.num_nodes, 1))
+        data.id = torch.tensor(np.random.permutation(np.arange(data.num_nodes))).unsqueeze(1)
+        return data
+
+    def makedata(self):
+        pass
+
+
+class LimitsOne(SymmetrySet):
+    def __init__(self):
+        super().__init__()
+        self.hidden_units = 16
+        self.num_classes = 2
+        self.num_features = 4
+        self.num_nodes = 8
+        self.graph_class = False
+
+    def makedata(self):
+        n_nodes = 16  # There are two connected components, each with 8 nodes
+
+        ports = [1, 1, 2, 2] * 8
+        colors = [0, 1, 2, 3] * 4
+
+        y = torch.tensor([0] * 8 + [1] * 8)
+        edge_index = torch.tensor([[0, 1, 1, 2, 2, 3, 3, 0, 4, 5, 5, 6, 6, 7, 7, 4, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13,
+                                    13, 14, 14, 15, 15, 8],
+                                   [1, 0, 2, 1, 3, 2, 0, 3, 5, 4, 6, 5, 7, 6, 4, 7, 9, 8, 10, 9, 11, 10, 12, 11, 13, 12,
+                                    14, 13, 15, 14, 8, 15]], dtype=torch.long)
+        x = torch.zeros((n_nodes, 4))
+        x[range(n_nodes), colors] = 1
+
+        data = Data(x=x, edge_index=edge_index, y=y)
+        data.id = torch.tensor(np.random.permutation(np.arange(n_nodes))).unsqueeze(1)
+        data.ports = torch.tensor(ports).unsqueeze(1)
+        data.train_mask = torch.ones(16, dtype=torch.bool)  # All nodes are in the train set
+        data.test_mask = torch.ones(16, dtype=torch.bool)  # All nodes are also in the test set
+        data.val_mask = torch.ones(16, dtype=torch.bool)
+        return [data]
+
+
+class LimitsTwo(SymmetrySet):
+    def __init__(self):
+        super().__init__()
+        self.hidden_units = 16
+        self.num_classes = 2
+        self.num_features = 4
+        self.num_nodes = 8
+        self.graph_class = False
+
+    def makedata(self):
+        n_nodes = 16  # There are two connected components, each with 8 nodes
+
+        ports = ([1, 1, 2, 2, 1, 1, 2, 2] * 2 + [3, 3, 3, 3]) * 2
+        colors = [0, 1, 2, 3] * 4
+        y = torch.tensor([0] * 8 + [1] * 8)
+        edge_index = torch.tensor([[0, 1, 1, 2, 2, 3, 3, 0, 4, 5, 5, 6, 6, 7, 7, 4, 1, 3, 5, 7, 8, 9, 9, 10, 10, 11, 11,
+                                    8, 12, 13, 13, 14, 14, 15, 15, 12, 9, 15, 11, 13],
+                                   [1, 0, 2, 1, 3, 2, 0, 3, 5, 4, 6, 5, 7, 6, 4, 7, 3, 1, 7, 5, 9, 8, 10, 9, 11, 10, 8,
+                                    11, 13, 12, 14, 13, 15, 14, 12, 15, 15, 9, 13, 11]], dtype=torch.long)
+        x = torch.zeros((n_nodes, 4))
+        x[range(n_nodes), colors] = 1
+
+        data = Data(x=x, edge_index=edge_index, y=y)
+        data.id = torch.tensor(np.random.permutation(np.arange(n_nodes))).unsqueeze(1)
+        data.ports = torch.tensor(ports).unsqueeze(1)
+        data.train_mask = torch.ones(16, dtype=torch.bool)  # All nodes are in the train set
+        data.test_mask = torch.ones(16, dtype=torch.bool)  # All nodes are also in the test set
+        data.val_mask = torch.ones(16, dtype=torch.bool)
+        return [data]
+
+
+class Triangles(SymmetrySet):
+    def __init__(self):
+        super().__init__()
+        self.hidden_units = 16
+        self.num_classes = 2
+        self.num_features = 1
+        self.num_nodes = 60
+        self.graph_class = False
+
+    def makedata(self):
+        size = self.num_nodes
+        generated = False
+        while not generated:
+            nx_g = nx.random_degree_sequence_graph([3] * size)
+            data = from_networkx(nx_g)
+            labels = [0] * size
+            for n in range(size):
+                for nb1 in data.edge_index[1][data.edge_index[0] == n]:
+                    for nb2 in data.edge_index[1][data.edge_index[0] == n]:
+                        if torch.logical_and(data.edge_index[0] == nb1, data.edge_index[1] == nb2).any():
+                            labels[n] = 1
+            generated = labels.count(0) >= 20 and labels.count(1) >= 20
+        data.y = torch.tensor(labels)
+
+        data = self.addports(data)
+        data = self.makefeatures(data)
+        data.train_mask = torch.ones(60, dtype=torch.bool)  # All nodes are in the train set
+        data.test_mask = torch.ones(60, dtype=torch.bool)  # All nodes are also in the test set
+        data.val_mask = torch.ones(60, dtype=torch.bool)
+        return [data]
+
+
+class LCC(SymmetrySet):
+    def __init__(self):
+        super().__init__()
+        self.hidden_units = 16
+        self.num_classes = 3
+        self.num_features = 1
+        self.num_nodes = 10
+        self.graph_class = False
+
+    def makedata(self):
+        generated = False
+        while not generated:
+            graphs = []
+            labels = []
+            i = 0
+            while i < 6:
+                size = 10
+                nx_g = nx.random_degree_sequence_graph([3] * size)
+                if nx.is_connected(nx_g):
+                    i += 1
+                    data = from_networkx(nx_g)
+                    lbls = [0] * size
+                    for n in range(size):
+                        edges = 0
+                        nbs = [int(nb) for nb in data.edge_index[1][data.edge_index[0] == n]]
+                        for nb1 in nbs:
+                            for nb2 in nbs:
+                                if torch.logical_and(data.edge_index[0] == nb1, data.edge_index[1] == nb2).any():
+                                    edges += 1
+                        lbls[n] = int(edges / 2)
+                    data.y = torch.tensor(lbls)
+                    labels.extend(lbls)
+                    data = self.addports(data)
+                    data = self.makefeatures(data)
+                    graphs.append(data)
+            generated = labels.count(0) >= 10 and labels.count(1) >= 10 and labels.count(
+                2) >= 10  # Ensure the dataset is somewhat balanced
+
+        return graphs
+
+
+class FourCycles(SymmetrySet):
+    def __init__(self):
+        super().__init__()
+        self.p = 4
+        self.hidden_units = 16
+        self.num_classes = 2
+        self.num_features = 1
+        self.num_nodes = 4 * self.p
+        self.graph_class = True
+
+    def gen_graph(self, p):
+        edge_index = None
+        for i in range(p):
+            e = torch.tensor([[i, p + i, 2 * p + i, 3 * p + i], [2 * p + i, 3 * p + i, i, p + i]], dtype=torch.long)
+            if edge_index is None:
+                edge_index = e
+            else:
+                edge_index = torch.cat([edge_index, e], dim=-1)
+        top = np.zeros((p * p,))
+        perm = np.random.permutation(range(p))
+        for i, t in enumerate(perm):
+            top[i * p + t] = 1
+        bottom = np.zeros((p * p,))
+        perm = np.random.permutation(range(p))
+        for i, t in enumerate(perm):
+            bottom[i * p + t] = 1
+        for i, bit in enumerate(top):
+            if bit:
+                e = torch.tensor([[i // p, p + i % p], [p + i % p, i // p]], dtype=torch.long)
+                edge_index = torch.cat([edge_index, e], dim=-1)
+        for i, bit in enumerate(bottom):
+            if bit:
+                e = torch.tensor([[2 * p + i // p, 3 * p + i % p], [3 * p + i % p, 2 * p + i // p]], dtype=torch.long)
+                edge_index = torch.cat([edge_index, e], dim=-1)
+        return Data(edge_index=edge_index, num_nodes=self.num_nodes), any(np.logical_and(top, bottom))
+
+    def makedata(self):
+        size = 25
+        p = self.p
+        trues = []
+        falses = []
+        while len(trues) < size or len(falses) < size:
+            data, label = self.gen_graph(p)
+            data = self.makefeatures(data)
+            data = self.addports(data)
+            data.y = label
+            if label and len(trues) < size:
+                trues.append(data)
+            elif not label and len(falses) < size:
+                falses.append(data)
+        return trues + falses
+
+
+class SkipCircles(SymmetrySet):
+    def __init__(self):
+        super().__init__()
+        self.hidden_units = 32
+        self.num_classes = 10  # num skips
+        self.num_features = 1
+        self.num_nodes = 41
+        self.graph_class = True
+        self.makedata()
+
+    def makedata(self):
+        size = self.num_nodes
+        skips = [2, 3, 4, 5, 6, 9, 11, 12, 13, 16]
+        graphs = []
+        for s, skip in enumerate(skips):
+            edge_index = torch.tensor([[0, size - 1], [size - 1, 0]], dtype=torch.long)
+            for i in range(size - 1):
+                e = torch.tensor([[i, i + 1], [i + 1, i]], dtype=torch.long)
+                edge_index = torch.cat([edge_index, e], dim=-1)
+            for i in range(size):
+                e = torch.tensor([[i, i], [(i - skip) % size, (i + skip) % size]], dtype=torch.long)
+                edge_index = torch.cat([edge_index, e], dim=-1)
+            data = Data(edge_index=edge_index, num_nodes=self.num_nodes)
+            data = self.makefeatures(data)
+            data = self.addports(data)
+            data.y = torch.tensor(s)
+            graphs.append(data)
+
+        return graphs
+
+
+
 def log_loaded_dataset(dataset, format, name):
     logging.info(f"[*] Loaded dataset '{name}' from '{format}':")
     logging.info(f"  {dataset.data}")
@@ -265,6 +520,9 @@ def load_dataset_master(format, name, dataset_dir):
 
         else:
             raise ValueError(f"Unexpected PyG Dataset identifier: {format}")
+
+    elif format == 'Synthetic':
+        dataset = preformat_syntheic(dataset_dir, name)
 
     # GraphGym default loader for Pytorch Geometric datasets
     elif format == 'PyG':
@@ -616,6 +874,10 @@ def add_pe_transform_to_dataset(format, name, dataset_dir, pe_transform=None):
         else:
             raise ValueError(f"Unexpected PyG Dataset identifier: {format}")
 
+    elif format == 'Synthetic':
+        pyg_dataset_id = format.split('-', 1)[1]
+        dataset = preformat_syntheic(dataset_dir, pyg_dataset_id)
+
     # GraphGym default loader for Pytorch Geometric datasets
     elif format == 'PyG':
         dataset = load_pyg(name, dataset_dir)
@@ -655,8 +917,6 @@ def add_pe_transform_to_dataset(format, name, dataset_dir, pe_transform=None):
 
 
 
-
-
 def compute_indegree_histogram(dataset):
     """Compute histogram of in-degree of nodes needed for PNAConv.
 
@@ -676,6 +936,85 @@ def compute_indegree_histogram(dataset):
         max_degree = max(max_degree, d.max().item())
         deg += torch.bincount(d, minlength=deg.numel())
     return deg.numpy().tolist()[:max_degree + 1]
+
+from torch_geometric.data import InMemoryDataset, Data
+
+class SyntheticGraphDataset(InMemoryDataset):
+    def __init__(self, root, data_list=None, transform=None, pre_transform=None, split = "train"):
+        super().__init__(root, transform, pre_transform)
+        self.split = split
+        if data_list is not None:
+            self.data, self.slices = self.collate(data_list)
+        else:
+            self.data, self.slices = None, None
+
+    def _download(self):
+        pass  # No download needed
+
+    def _process(self):
+        pass  # No processing needed
+
+class SyntheticNodeDataset(InMemoryDataset):
+    def __init__(self, root, data_list=None, transform=None, pre_transform=None):
+        self.data_list = data_list
+        super().__init__(root, transform, pre_transform)
+        self.data, self.slices = self.collate(self.data_list)
+
+    @property
+    def raw_file_names(self):
+        return []
+
+    @property
+    def processed_file_names(self):
+        return []
+
+    def download(self):
+        pass
+
+    def process(self):
+        pass
+
+
+def preformat_syntheic(dataset_dir, name):
+    """Load and preformat datasets from Synthetic Datasets of DropGNN.
+        https://arxiv.org/pdf/2111.06283
+    Args:
+        dataset_dir: path where to store the cached dataset
+        name: name of the specific dataset in the Synthetic Datasets
+
+    Returns:
+        PyG dataset object
+    """
+    dataset = SkipCircles()
+    if name == "skipcircles":
+        dataset = SkipCircles()
+    elif name == "triangles":
+        dataset = Triangles()
+    elif name == "limitsone":
+        dataset = LimitsOne()
+    elif name == "limitstwo":
+        dataset = LimitsTwo()
+    elif name == "fourcycles":
+        dataset = FourCycles()
+
+
+    if name in ['skipcircles', 'fourcycles']:
+        dataset_splits = ['train', 'val', 'test']
+        datasets = []
+        for split in dataset_splits:
+            data_list = dataset.makedata()  # Randomized data for each split
+
+            syn_dataset = SyntheticGraphDataset(root=dataset_dir, split=split, data_list=data_list)
+            datasets.append(syn_dataset)
+        dataset = join_dataset_splits(
+            datasets
+        )
+        return dataset
+    else:
+        syn_dataset = SyntheticNodeDataset(root=dataset_dir, data_list=dataset.makedata())
+        return syn_dataset
+
+
 
 
 def preformat_GNNBenchmarkDataset(dataset_dir, name):
@@ -704,6 +1043,8 @@ def preformat_GNNBenchmarkDataset(dataset_dir, name):
     pre_transform_in_memory(dataset, T.Compose(tf_list))
 
     return dataset
+
+
 
 
 def preformat_MalNetTiny(dataset_dir, feature_set):
